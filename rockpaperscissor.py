3d612e5ea497d3238aaa63f2af3f458587c9159d
Dhiwahar K Adhithya <75292659+DHIWAHAR-K@users.noreply.github.com>
2024-02-19 00:45:49 +0530

    Update rockpaperscissor.py

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -4,7 +4,7 @@ import random
 root = Tk()
 root.geometry('400x400')
 root.resizable(0,0)
-root.title('DataFlair-Rock,Paper,Scissors')
+root.title('Rock,Paper,Scissors')
 root.config(bg ='seashell3')
 
 Label(root, text = 'Rock, Paper ,Scissors' , font='arial 20 bold', bg = 'seashell2').pack()
